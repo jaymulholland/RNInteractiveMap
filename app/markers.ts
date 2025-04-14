e4d8ce008dbf8331391ad2a11af4d7073ccc0041
jaymulholland <jay.mulholland.7@gmail.com>
2025-04-14 11:34:33 +0100

    initial commit

---
+++ app/markers.ts
@@ -0,0 +1,20 @@
+export type MarkerType = {
+    name: string;
+    latitude: number;
+    longitude: number;
+  };
+  
+  export const markers: MarkerType[] = [
+    {
+      name: 'London',
+      latitude: 51.5072,
+      longitude: -0.1276
+    },
+    {
+      name: 'San Francisco',
+      latitude: 37.7749,
+      longitude: -122.4194
+    }
+    // Add more markers here
+  ];
+  
